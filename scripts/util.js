0f777b59fa6d1161e9d841d6dbf0d8785740da23
Scott Sutherland <55766484+scottasut@users.noreply.github.com>
2021-02-20 18:50:16 -0800

    Update util.js

--- scripts/util.js
+++ scripts/util.js
@@ -1,3 +1,23 @@
 function capitalize(s) {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
+
+// The following are intended to interpret the maps API journey length
+// \/\/\/                 and distance fields                   \/\/\/
+
+// Returns distance in miles
+function parseDistance(distance) {
+    return parseInt(distance.replace(/\D/g, ""));
+}
+
+// Returns time in hours
+function parseTime(time) {
+    let tokens = time.split(" ");
+    tokens = tokens.filter(token => token.length && token == +token);
+
+    if (tokens.length == 1) {
+        return tokens[0];
+    }
+
+    return tokens[0] + (tokens[1] / 60);
+}
